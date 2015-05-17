3bba8d200c45ba1e1e8e3c52e63bff0b40e87326
william <1041897388@qq.com>
2015-05-17 12:51:00 +0800

    add commander

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+#!/usr/bin/env node
 'use strict';
 
 var Q = require('q');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
 var fse = require('fs-extra');
+var program = require('commander')
+
+
+program
+	.version('1.0.0')
+	.option('-s,--save','save data into file')
+	.parse(process.argv);
+
+if(program.save){
+	save(process.argv[3]);
+}
 
 /**
  * cache of provinces name
@@ -12,7 +24,7 @@ var fse = require('fs-extra');
 var cache = [];
 /**
  * fetch data from webpage
- * @return {Array} [result data]
+ * @return {result} [result data]
  */
 function fetch(){
 	
@@ -20,13 +32,13 @@ function fetch(){
 	var deferred = Q.defer();
 	var taiwan = '台湾省';
 	var taiwanCity = ['新北市','台北市','台中市','台南市','高雄市','基隆市','嘉义市','屏东市',
-	 		  '台北县板桥市','三重市','中和市','永和市','新庄市','新店市','土城市',
-			  '芦洲市','树林市','汐止市','台中县丰原市','大里市','太平市','台南县新营市',
-			  '永康市','高雄县凤山市'];
+					  '台北县板桥市','三重市','中和市','永和市','新庄市','新店市','土城市',
+					  '芦洲市','树林市','汐止市','台中县丰原市','大里市','太平市','台南县新营市',
+					  '永康市','高雄县凤山市'];
 	var xianggang = '香港特别行政区';
 	var xianggangCity = ['中西区','东区','南区','湾仔区','九龙城区','观塘区','深水埗区',
-  			     '黄大仙区','油尖旺区','离岛区','葵青区','北区','西贡区','沙田区',
-			     '大埔区','荃湾区','屯门区','元朗区'];
+						 '黄大仙区','油尖旺区','离岛区','葵青区','北区','西贡区','沙田区',
+						 '大埔区','荃湾区','屯门区','元朗区'];
 	var aomen = '澳门特别行政区';
 	var aomenCity = '澳门市';
 	superagent.get(url)
@@ -38,7 +50,7 @@ function fetch(){
 			var $ = cheerio.load(data.text);
 			var result = [];
 
-			$('.addlist').each(function(index,ele){		           // fetch data from website		
+			$('.addlist').each(function(index,ele){				
 				$(ele).find('tr').each(function(index,tr){
 
 					var re = /\r\n|\s+/g;
@@ -54,20 +66,20 @@ function fetch(){
 				});
 			});
 
-			for(var i = 0;i<taiwanCity.length;i++){                   // add taiwan 
+			for(var i = 0;i<taiwanCity.length;i++){
 				result.push({
 					province:taiwan,
 					city:taiwanCity[i]
 				});
 			}
 
-			for(var i = 0;i<xianggangCity.length;i++){               // add xianggang
+			for(var i = 0;i<xianggangCity.length;i++){
 				result.push({
 					province:xianggang,
 					city:xianggangCity[i]
 				});
 			}
-			result.push({                                           // add aomen
+			result.push({
 				province:aomen,
 				city:aomenCity
 			});
@@ -80,11 +92,12 @@ function fetch(){
 /**
  * save data in json
  * @param  {String} path [file path]
- * @param  {All} hint [display the hint or not]
  */
-module.exports.save = function(path,hint){                                     // save data as json file
-
-	fetch()
+function save(path){
+	if(!path){
+		console.log('specific file path !');
+	}else{
+		fetch()
 		.then(function(data){
 			return JSON.stringify(data);
 		})
@@ -92,15 +105,12 @@ module.exports.save = function(path,hint){                                     /
 			fse.outputJson(path,json);
 		})
 		.then(function(data){
-			if(hint === null || hint ===undefined ||hint === true){
 				console.log('save file success. file path : ',path)
-			}
 		},function(err){
-			if(hint === null || hint ===undefined ||hint === true){
 				console.log('save file failure.')		
-			}
 			console.log(err);
 		});
+	}
 };
 
 /**
@@ -108,7 +118,7 @@ module.exports.save = function(path,hint){                                     /
  * @param  {String} path [file path]
  * @return {Array}      [array of data]
  */
-module.exports.getAll = function(path){                                         // get all data
+module.exports.getAll = function(path){
 	return JSON.parse(fse.readJsonSync(path));
 };
 
@@ -119,9 +129,9 @@ module.exports.getAll = function(path){                                         
  */
 module.exports.getProvinceName = function(path){
 
-	if(cache && cache.length){    						// cached province name
+	if(cache && cache.length){    				// cached province name
 		return cache;
-	}else{									// no cached province name
+	}else{										// no cached province name
 		var data = JSON.parse(fse.readJsonSync(path));
 		cache.push(data[0].province);
 
@@ -147,7 +157,7 @@ module.exports.getCityNameByProvince = function(path,index){
 	var data = JSON.parse(fse.readJsonSync(path));
 	var result = [];
 	var index = index - 1;
-	if(cache && cache.length){                  				// cached province name
+	if(cache && cache.length){                  // cached province name
 		for(var i = 0;i<data.length;i++){
 			if(data[i].province == cache[index]){
 				result.push(data[i].city);		
@@ -174,9 +184,6 @@ module.exports.getCityNameByProvince = function(path,index){
 		return result;
 	}
 };
-
-
-
 
 
 
